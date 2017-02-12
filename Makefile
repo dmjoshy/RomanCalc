08c872b0193ec6ea4a023ca60ca06a9a54d3af85
dmjoshy <dmjoshy@bn16-198.dcs.mcnc.org>
2017-02-12 10:49:35 -0500

    0.5 Green on Basic Add

--- Makefile
+++ Makefile
@@ -6,4 +6,4 @@ clean:
 	@rm -f *.o 
 
 test: test.c RomanCalc.c
-	@gcc -o test test.c RomanCalc.c
+	@gcc -o test test.c RomanCalc.c  `pkg-config --cflags --libs check`
--- test.c
+++ test.c
@@ -12,13 +12,43 @@
  *****************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <check.h>
 #include "RomanCalc.h"
 
-void main(int argc, char **argv)
+START_TEST (test_basic_add)
 {
-	char *s;
-	s = (char *) malloc(30);
-	RomanAdd("XXXV","XLII",s);
-  	//RomanSub("MMCMXIX","I",s);
-  	printf("Result=%s\n",s);
+  	char a[10]="I";
+  	char b[10]="I";
+  	char c[10];
+  	RomanAdd(a,b,c);
+  	ck_assert_str_eq(c, "II");
+}
+END_TEST
+
+Suite * roman_suite(void)
+{
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("Roman");
+
+    /* Core test case */
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, test_basic_add);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+int main(void)
+{
+	int number_failed;
+    Suite *s;
+    SRunner *sr;
+    s = roman_suite();
+    sr = srunner_create(s);
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
